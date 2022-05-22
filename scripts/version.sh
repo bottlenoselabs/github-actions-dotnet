1acbb24cb19af7e843a7b5782332580aa147f35a
Lucas Girouard-Stranks <lustranks@gmail.com>
2022-05-21 21:43:35 -0400

    Update `version.sh`

--- scripts/version.sh
+++ scripts/version.sh
@@ -16,12 +16,26 @@ fi
 
 echo "Getting SemVer2 for the Git repository '$1'..."
 
-DOTNET_APP_VERSION=`~/.dotnet/tools/dotnet-gitversion /output json /showvariable NuGetVersionV2`
+uname_str="$(uname -a)"
+case "${uname_str}" in
+    *Microsoft*)    TARGET_BUILD_OS="windows";;
+    *microsoft*)    TARGET_BUILD_OS="windows";;
+    Linux*)         TARGET_BUILD_OS="linux";;
+    Darwin*)        TARGET_BUILD_OS="macos";;
+    CYGWIN*)        TARGET_BUILD_OS="linux";;
+    MINGW*)         TARGET_BUILD_OS="windows";;
+    *Msys)          TARGET_BUILD_OS="windows";;
+    *)              TARGET_BUILD_OS="UNKNOWN:${uname_str}"
+esac
 
-# # GitVersion for Windows is broken; it only returns a JSON result no matter what... Have to parse value out of JSON using Python: https://stackoverflow.com/questions/1955505/parsing-json-with-unix-tools
-# ~/.dotnet/tools/dotnet-gitversion $1 /output json >> $1/version.json
-# DOTNET_APP_VERSION=`python3 -c "import sys, json; print(json.load(open(sys.argv[1]))['NuGetVersionV2'])" $1/version.json`
-# rm -rf $1/version.json
+if [[ "$TARGET_BUILD_OS" == "windows" ]]; then
+    # GitVersion for Windows is broken; it only returns a JSON result no matter what... Have to parse value out of JSON using Python: https://stackoverflow.com/questions/1955505/parsing-json-with-unix-tools
+    ~/.dotnet/tools/dotnet-gitversion $1 /output json >> $1/version.json
+    DOTNET_APP_VERSION=`python -c "import sys, json; print(json.load(open(sys.argv[1]))['NuGetVersionV2'])" $1/version.json`
+    rm -rf $1/version.json
+else
+    DOTNET_APP_VERSION=`~/.dotnet/tools/dotnet-gitversion /output json /showvariable NuGetVersionV2`
+fi    
 
 echo "Getting SemVer2 for the Git repository '$1': Finished. Version: '$DOTNET_APP_VERSION'."
 
