255636fc4cf926bd2a7e835c8f97e02758552fe1
Lucas Girouard-Stranks <lustranks@gmail.com>
2022-05-21 21:25:18 -0400

    Update version.sh

--- scripts/version.sh
+++ scripts/version.sh
@@ -18,7 +18,7 @@ echo "Getting SemVer2 for the Git repository '$1'..."
 
 # GitVersion for Windows is broken; it only returns a JSON result no matter what... Have to parse value out of JSON using Python: https://stackoverflow.com/questions/1955505/parsing-json-with-unix-tools
 ~/.dotnet/tools/dotnet-gitversion $1 /output json >> $1/version.json
-DOTNET_APP_VERSION=`python -c "import sys, json; print(json.load(open(sys.argv[1]))['NuGetVersionV2'])" $1/version.json`
+DOTNET_APP_VERSION=`python3 -c "import sys, json; print(json.load(open(sys.argv[1]))['NuGetVersionV2'])" $1/version.json`
 rm -rf $1/version.json
 echo "Getting SemVer2 for the Git repository '$1': Finished. Version: '$DOTNET_APP_VERSION'."
 
